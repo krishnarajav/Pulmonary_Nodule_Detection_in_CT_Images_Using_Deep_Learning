7fa2f660da57b0dc0c5d090ce029b4f26900f52d
krishnarajav <118679169+krishnarajav@users.noreply.github.com>
2024-11-28 15:24:48 +0530

    updated ui

--- Web App/NoduleNet/system1/urls.py
+++ Web App/NoduleNet/system1/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('home', views.home, name='home'),
     path('about', views.about, name='about'),
     path('detect', views.detect, name='detect'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Web App/NoduleNet/system1/views.py
+++ Web App/NoduleNet/system1/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.conf import settings
+import os
 
 def home(request):
     return render(request, 'home.html')
@@ -7,4 +9,23 @@ def about(request):
     return render(request, 'about.html')
 
 def detect(request):
-    return render(request, 'detect.html')
+    display_img = None
+
+    if request.method == "POST" and request.FILES.get('image'):
+        # Get the uploaded image
+        uploaded_image = request.FILES['image']
+
+        # Define the path where you want to save the image
+        upload_dir = os.path.join(settings.MEDIA_ROOT, 'uploads')
+        os.makedirs(upload_dir, exist_ok=True)  # Ensure the directory exists
+        image_path = os.path.join(upload_dir, uploaded_image.name)
+
+        # Save the image
+        with open(image_path, 'wb') as f:
+            for chunk in uploaded_image.chunks():
+                f.write(chunk)
+
+        # Prepare the path to the image to pass to the template
+        display_img = os.path.join(settings.MEDIA_URL, 'uploads', uploaded_image.name)
+
+    return render(request, 'detect.html', {'display_img': display_img})
